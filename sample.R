6867daf67f60abd9b89a4db36a6ec5a40c62bc50
pandeymanishh <pandey.manishh@gmail.com>
2018-01-31 10:09:05 +0000

    Sample to create shiny semantic tables

---
+++ sample.R
@@ -0,0 +1,56 @@
+library(shiny)
+library(shiny.semantic)
+library(shinyjs)
+ui <- function() {  
+  shinyUI(    
+  semanticPage(      
+  shinyjs::useShinyjs(),      
+  tags$head(tags$script(src="tablesort.js")),     
+  title = "My page",
+   suppressDependencies("bootstrap"),     
+   p(),      
+   h4("Table with the class 'ui red table'"),      
+   HTML('<table class="ui red table"> 
+   <thead>    <tr>
+   <th>Food</th>           
+   <th>Calories</th>            
+   <th>Protein</th>           
+   </tr><tbody>            
+   <tr>            
+   <td>Apples</td>           
+   <td>200</td>            
+   <td>0g</td>            
+   </tr>           
+   <tr>            
+   <td>Orange</td>           
+   <td>310</td>            
+   <td>0g</td>            
+   </tr>           
+   </tbody>           
+   </table>'),            
+   p(),      
+   h4("Table with the class 'ui sortable celled table'"),      
+   HTML('<table class="ui sortable celled table">  
+   <thead>    
+   <tr><th>Food</th>            
+   <th>Calories</th>            
+   <th>Protein</th>            
+   </tr><tbody>            
+   <tr>           
+   <td>Apples</td>           
+   <td>200</td>            
+   <td>0g</td>            
+   </tr>            
+   <tr>            
+   <td>Orange</td>            
+   <td>310</td>           
+   <td>0g</td>           
+   </tr>            
+   </tbody>            
+   </table>')    )  
+   )}
+   
+server <- shinyServer(function(input, output) {
+  runjs("$('table').tablesort();")
+} )  
+shinyApp(ui = ui(), server = server)
